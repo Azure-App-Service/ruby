bbbe8fb0ff3ad958aa018f6d41785a37ffdb2f61
James Christianson <jamesrchristianson@gmail.com>
2017-04-25 10:41:46 -0700

    Update init_container.sh

--- 2.3-2/init_container.sh
+++ 2.3-2/init_container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 service ssh start
 touch /home/LogFiles/ruby_$WEBSITE_ROLE_INSTANCE_ID_out.log
-echo "$(date) Container started" >> /home/LogFiles/ruby_$WEBSITE_ROLE_INSTANCE_ID_out.log
+echo "$(date) Container started, server output will be sent to docker logs" >> /home/LogFiles/ruby_$WEBSITE_ROLE_INSTANCE_ID_out.log
+bash /opt/startup.sh
 
