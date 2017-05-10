bfbd42cc708ad85dfca083f8f2a510bc9f0543d4
James Christianson <jamesrchristianson@gmail.com>
2017-05-10 15:11:11 -0700

    Update init_container.sh

--- 2.3-2/init_container.sh
+++ 2.3-2/init_container.sh
@@ -2,5 +2,7 @@
 service ssh start
 touch /home/LogFiles/ruby_${WEBSITE_ROLE_INSTANCE_ID}_out.log
 echo "$(date) Container started, server output will be sent to docker logs" >> /home/LogFiles/ruby_${WEBSITE_ROLE_INSTANCE_ID}_out.log
+eval "$(rbenv init -)"
+rbenv global 2.3.3
 bash /opt/startup.sh
 
