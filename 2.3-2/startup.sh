e0c388b17de4bb5845d5e88ee5553faea8a7286c
James Christianson <jamesrchristianson@gmail.com>
2017-05-16 16:34:45 -0700

    Update startup.sh
    Add variables for absolute paths, correlating with Kuduscript

--- 2.3-2/startup.sh
+++ 2.3-2/startup.sh
@@ -1,3 +1,28 @@
+# initialize env
+if [ -z $BUNDLE_WITHOUT ]; then 
+  echo "Bundle install with no 'without' options"; 
+  RUBY_OPTIONS="";
+else 
+  RUBY_OPTIONS="--without $BUNDLE_WITHOUT";
+  echo "Bundle install with options $RUBY_OPTIONS";
+fi
+
+if [ -z $BUNDLE_INSTALL_LOCATION ]; then 
+  echo "Defaulting gem installation directory to /tmp/bundle"; 
+  BUNDLE_INSTALL_LOCATION="/tmp/bundle";
+else 
+  echo "Gem installation directory is $BUNDLE_INSTALL_LOCATION";
+fi
+
+if [ -z $RUBY_SITE_CONFIG_DIR ]; then 
+  echo "Defaulting site config directory to /home/site/config"; 
+  RUBY_SITE_CONFIG_DIR="/home/site/config"
+else 
+  echo "site config directory is $RUBY_SITE_CONFIG_DIR";
+fi
+	
+
+
 if [ -e /home/site/wwwroot/Gemfile ] 
   then
     echo 'Found Gemfile, working directory is /home/site/wwwroot'
@@ -24,7 +49,7 @@ fi
 
 if [ -n "$RAILS_ENV" ]
   then
-    echo 'RAILS_ENV set to $RAILS_ENV'
+    echo "RAILS_ENV set to $RAILS_ENV"
   else
     echo 'RAILS_ENV not set, default to production'
     export RAILS_ENV='production'
@@ -34,16 +59,17 @@ echo 'Removing any leftover pids if present'
 rm -f tmp/pids/* ;
 
 # Support zipped gems 
-if [ -f /home/site/config/gems.tgz ]
+
+if [ -f  ${RUBY_SITE_CONFIG_DIR}/gems.tgz ]
   then
     echo "gems.tgz detected, beginning unzipping process"
     echo "unzipping..."
-    mkdir -p /tmp/bundle
-    cp /home/site/config/gems.tgz /tmp
-    tar -C /tmp/bundle -xf /tmp/gems.tgz
+    mkdir -p $BUNDLE_INSTALL_LOCATION
+    cp  ${RUBY_SITE_CONFIG_DIR}/gems.tgz /tmp
+    tar -C $BUNDLE_INSTALL_LOCATION -xf /tmp/gems.tgz
     
     echo 'Removing bundler config'
-    rm -f /tmp/bundle/config
+    rm -f ${BUNDLE_INSTALL_LOCATION}/config
     
     export ZIPPED_GEMS=1
 fi
@@ -51,7 +77,7 @@ fi
 echo 'Running bundle check'
 if [ $ZIPPED_GEMS -eq 1 ]
   then
-    bundle config --global path /tmp/bundle
+    bundle config --global path $BUNDLE_INSTALL_LOCATION
     if bundle check | grep satisfied
     then
       echo 'dependency check passed'
@@ -72,11 +98,11 @@ fi
 
 if [ $ZIPPED_GEMS -eq 1 ]
   then
-    echo 'running bundle install --no-deployment'
-    bundle install --no-deployment
+    echo "running bundle install $RUBY_OPTIONS --no-deployment"
+    bundle install --no-deployment $RUBY_OPTIONS
   else
-    echo 'running bundle install --local --path vendor/bundle'
-    bundle install --local --path vendor/bundle
+    echo "running bundle install $RUBY_OPTIONS --local --path vendor/bundle"
+    bundle install  $RUBY_OPTIONS --local --path vendor/bundle
 fi
 
 if [ -n "$GEM_PRISTINE" ]
@@ -87,7 +113,7 @@ fi
 
 if [ -n "$PORT" ]
   then
-    echo 'Using Port $PORT'
+    echo "Using Port $PORT"
   else
     echo 'Defaulting to port 3000'
     export PORT=3000
@@ -95,9 +121,9 @@ fi
 
 if [ -n "$APP_COMMAND_LINE" ]
   then
-    echo 'using command: $APP_COMMAND_LINE'
+    echo "using command: $APP_COMMAND_LINE"
   else
-    echo 'defaulting to command: "bundle exec rails server -e $RAILS_ENV -p $PORT"'
+    echo "defaulting to command: \"bundle exec rails server -e $RAILS_ENV -p $PORT\""
     export APP_COMMAND_LINE="bundle exec rails server -b 0.0.0.0 -e $RAILS_ENV -p $PORT"
 fi
 
