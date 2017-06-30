04215f1295451c2ce5902e812def27425ee8a0ff
Nick Walker <nickwalk@microsoft.com>
2017-06-29 18:05:55 -0700

    Dir name revision, build script

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -x -e
 
-docker build -t "$1"/rubybase:2.3.3 base_images/2.3.3
-docker build -t "$1"/ruby:2.3.3-2 -t "$1"/ruby:2.3-2 -t "$1"/ruby:2-2 -t "$1"/ruby:latest 2.3
+buildnumber=${4-$(date -u +"%y%m%d%H%M")}
+
+#docker build -t "$1"/rubybase:2.3.3_"$buildnumber" base_images/2.3.3
+docker build -t "$1"/ruby:2.3.3_"$buildnumber" -t "$1"/ruby:latest_"$buildnumber" 2.3.3
 
 docker login -u "$2" -p "$3"
 
-docker push "$1"/rubybase:2.3.3
+#docker push "$1"/rubybase:2.3.3
 
-docker push "$1"/ruby:2.3.3-2
-docker push "$1"/ruby:2.3-2
-docker push "$1"/ruby:2-2
-docker push "$1"/ruby:latest
+docker push "$1"/ruby:latest_"$buildnumber"
+docker push "$1"/ruby:2.3.3_"$buildnumber"
 
 docker logout
